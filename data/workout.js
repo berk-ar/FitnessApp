6152c33be7f80897f91eba88892b5a2edcaeb445
Berk AR <berk.ar12@yandex.com>
2024-02-07 22:31:42 +0300

    Workout Screen added

---
+++ data/workout.js
@@ -0,0 +1,399 @@
+export default [
+    {
+        id: 0,
+        name: "FULL BODY",
+        description: "7x4 CHALLENGE",
+        image: "",
+        exercises: [
+            {
+                id: 10,
+                name: "Oblique twist",
+                image: "https://www.verywellfit.com/thmb/XMViMrZIc0mniQlbfrhe60XCGBM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/86-3120067-Seated-Oblique-twists-GIF-75816d0fb4c94494b254483a1fe1e365.gif",
+                sets: 12
+            },
+            {
+                id: 11,
+                name: "Benchpress",
+                image: "https://static.wixstatic.com/media/00b9a7_3af96dcc60fb46b08f1960cb53212b6a~mv2.gif",
+                sets: 12
+            },
+            {
+                id: 12,
+                name: "Deadlift",
+                image: "https://www.kettlebellkings.com/cdn/shop/articles/barbell-deadlift-movement.gif?v=1692228918",
+                sets: 12
+            },
+            {
+                id: 13,
+                name: "Squat",
+                image: "https://codahosted.io/docs/toQ738vXc0/blobs/bl-XSExo-oZTa/c05e276e16fd9514738dde20372561a358e7deba5594d27e4d840e2c8fb32396c58a2a2efc7ebe331c0e581dcffcab1b03007de93a092fc615fb6bb27ffcfe358291165b78e937f72c24da6c41fbdf3c0fa335207a4f3705ef2b85f07e677bc36ce36c1d",
+                sets: 12
+            },
+            {
+                id: 14,
+                name: "Tricep pulldown",
+                image: "https://vimeo.com/178054967",
+                sets: 12
+            },
+            {
+                id: 15,
+                name: "Pulley crunch",
+                image: "https://vimeo.com/178052126",
+                sets: 12
+            },
+            {
+                id: 16,
+                name: "Floor crunch",
+                image: "https://vimeo.com/178050912",
+                sets: 12
+            },
+            {
+                id: 17,
+                name: "Overhead tricep extension",
+                image: "https://vimeo.com/178050262",
+                sets: 12
+            },
+            {
+                id: 18,
+                name: "Dumbbell press",
+                image: "https://vimeo.com/178048727",
+                sets: 12
+            },
+            {
+                id: 19,
+                name: "Forearm curl back (15 reps)",
+                image: "https://vimeo.com/178047269",
+                sets: 12
+            },
+            {
+                id: 20,
+                name: "Forearm curl in (15 reps)",
+                image: "https://vimeo.com/178047269",
+                sets: 12
+            },
+            {
+                id: 21,
+                name: "Seated pulley row",
+                image: "https://vimeo.com/178042607",
+                sets: 12
+            },
+            {
+                id: 22,
+                name: "Shrug",
+                image: "https://vimeo.com/177971664",
+                sets: 12
+            },
+            {
+                id: 23,
+                name: "Calf raise",
+                image: "https://vimeo.com/177971348",
+                sets: 12
+            },
+            {
+                id: 24,
+                name: "Dumbbell romanian deadlift",
+                image: "https://vimeo.com/177971032",
+                sets: 12
+            },
+            {
+                id: 25,
+                name: "Forearm curl up (25 reps)",
+                image: "https://vimeo.com/177970644",
+                sets: 12
+            },
+            {
+                id: 26,
+                name: "Dumbbell squat",
+                image: "https://vimeo.com/177970342",
+                sets: 12
+            },
+            {
+                id: 27,
+                name: "Front raise",
+                image: "https://vimeo.com/177969758",
+                sets: 12
+            },
+            {
+                id: 28,
+                name: "Julian tricep extension",
+                image: "https://vimeo.com/177969524",
+                sets: 12
+            },
+            {
+                id: 29,
+                name: "Bicep curl",
+                image: "https://vimeo.com/177968823",
+                sets: 12
+            },
+            {
+                id: 30,
+                name: "Dumbbell incline press",
+                image: "https://vimeo.com/177967959",
+                sets: 12
+            },
+            {
+                id: 31,
+                name: "Hammer pulley curls 2h",
+                image: "https://vimeo.com/",
+                sets: 12
+            },
+            {
+                id: 32,
+                name: "Side raise",
+                image: "https://vimeo.com/177961354",
+                sets: 12
+            },
+            {
+                id: 33,
+                name: "Hand grippers",
+                image: "https://vimeo.com/177766222",
+                sets: 12
+            },
+            {
+                id: 34,
+                name: "Dumbbell shoulder press",
+                image: "https://www.vimeo.com/151248907",
+                sets: 12
+            },
+            {
+                id: 35,
+                name: "Overhead press",
+                image: "https://vimeo.com/96081016",
+                sets: 12
+            },
+            {
+                id: 36,
+                name: "Hamstring curl",
+                image: "https://vimeo.com/164443477",
+                sets: 12
+            },
+            {
+                id: 37,
+                name: "Lat pulldown",
+                image: "https://vimeo.com/164080300",
+                sets: 12
+            },
+            {
+                id: 38,
+                name: "Hanging leg raise",
+                image: "https://vimeo.com/39560703",
+                sets: 12
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: "ABS",
+        description: "WORK HARD",
+        image: "",
+        exercises: [
+            {
+                id: 20,
+                name: "Oblique twist",
+                image: "https://vimeo.com/178079508",
+                sets: 12
+            },
+            {
+                id: 21,
+                name: "Pulley crunch",
+                image: "https://vimeo.com/178052126",
+                sets: 12
+            },
+            {
+                id: 22,
+                name: "Floor crunch",
+                image: "https://vimeo.com/178050912",
+                sets: 12
+            },
+            {
+                id: 23,
+                name: "Hanging leg raise",
+                image: "https://vimeo.com/39560703",
+                sets: 12
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "CHEST",
+        description: "BOMB",
+        image: "",
+        exercises: [
+            {
+                id: 30,
+                name: "Benchpress",
+                image: "https://vimeo.com/178057019",
+                sets: 12
+            },
+            {
+                id: 31,
+                name: "Dumbbell press",
+                image: "https://vimeo.com/178048727",
+                sets: 12
+            },
+            {
+                id: 32,
+                name: "Front raise",
+                image: "https://vimeo.com/177969758",
+                sets: 12
+            },
+            {
+                id: 33,
+                name: "Dumbbell incline press",
+                image: "https://vimeo.com/177967959",
+                sets: 12
+            },
+        ]
+    },
+    {
+        id: 3,
+        name: "LEG",
+        description: "CR7",
+        image: "",
+        exercises: [
+            {
+                id: 40,
+                name: "Deadlift",
+                image: "https://vimeo.com/178056325",
+                sets: 12
+            }, {
+                id: 41,
+                name: "Squat",
+                image: "https://vimeo.com/178056008",
+                sets: 12
+            },
+            {
+                id: 42,
+                name: "Calf raise",
+                image: "https://vimeo.com/177971348",
+                sets: 12
+            },
+            {
+                id: 43,
+                name: "Dumbbell romanian deadlift",
+                image: "https://vimeo.com/177971032",
+                sets: 12
+            },
+            {
+                id: 44,
+                name: "Dumbbell squat",
+                image: "https://vimeo.com/177970342",
+                sets: 12
+            },
+            {
+                id: 45,
+                name: "Hamstring curl",
+                image: "https://vimeo.com/164443477",
+                sets: 12
+            },
+        ]
+    },
+    {
+        id: 4,
+        name: "ARM",
+        description: "ARNOLD",
+        image: "",
+        exercises: [
+            {
+                id: 4,
+                name: "Tricep pulldown",
+                image: "https://vimeo.com/178054967",
+                sets: 12
+            },
+            {
+                id: 7,
+                name: "Overhead tricep extension",
+                image: "https://vimeo.com/178050262",
+                sets: 12
+            },
+            {
+                id: 9,
+                name: "Forearm curl back (15 reps)",
+                image: "https://vimeo.com/178047269",
+                sets: 12
+            },
+            {
+                id: 10,
+                name: "Forearm curl in (15 reps)",
+                image: "https://vimeo.com/178047269",
+                sets: 12
+            },
+            {
+                id: 15,
+                name: "Forearm curl up (25 reps)",
+                image: "https://vimeo.com/177970644",
+                sets: 12
+            },
+            {
+                id: 18,
+                name: "Julian tricep extension",
+                image: "https://vimeo.com/177969524",
+                sets: 12
+            },
+            {
+                id: 19,
+                name: "Bicep curl",
+                image: "https://vimeo.com/177968823",
+                sets: 12
+            },
+            {
+                id: 21,
+                name: "Hammer pulley curls 2h",
+                image: "https://vimeo.com/",
+                sets: 12
+            },
+        ]
+    },
+    {
+        id: 5,
+        name: "BACK",
+        description: "....",
+        image: "",
+        exercises: [
+            {
+                id: 11,
+                name: "Seated pulley row",
+                image: "https://vimeo.com/178042607",
+                sets: 12
+            },
+            {
+                id: 12,
+                name: "Shrug",
+                image: "https://vimeo.com/177971664",
+                sets: 12
+            },
+            {
+                id: 27,
+                name: "Lat pulldown",
+                image: "https://vimeo.com/164080300",
+                sets: 12
+            },
+        ]
+    },
+    {
+        id: 6,
+        name: "SHOULDERS",
+        description: "....",
+        image: "",
+        exercises: [
+            {
+                id: 22,
+                name: "Side raise",
+                image: "https://vimeo.com/177961354",
+                sets: 12
+            },
+            {
+                id: 24,
+                name: "Dumbbell shoulder press",
+                image: "https://www.vimeo.com/151248907",
+                sets: 12
+            },
+            {
+                id: 25,
+                name: "Overhead press",
+                image: "https://vimeo.com/96081016",
+                sets: 12
+            },
+        ]
+    }
+]
---
+++ screens/WorkoutScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native"
+
+const WorkoutScreen = () => {
+    return (
+        <View>
+            <Text>WorkoutScreen</Text>
+        </View>
+    )
+}
+
+export default WorkoutScreen
+
+const styles = StyleSheet.create({
+
+})
